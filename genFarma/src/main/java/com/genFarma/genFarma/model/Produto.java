1a91c0c234f106cf7c0bd15f8f562fa47b6ebf73
Victor Carreras <titotortuga3@gmail.com>
2021-10-01 16:56:01 -0300

    [Feature Victor - Task Farma: CREATE modelCategoria] 2021.10.01

---
+++ genFarma/src/main/java/com/genFarma/genFarma/model/Produto.java
@@ -0,0 +1,79 @@
+package com.genFarma.genFarma.model;
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+/**
+ * @author Victor
+ * @version 1.0
+ */
+@Entity
+public class Produto {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long idProduto;
+
+	@NotBlank
+	@Size(min = 1, max = 100)
+	private String nomeProduto;
+
+	@NotBlank
+	@Size(min = 1, max = 300)
+	private String descricao;
+
+	@NotBlank
+	@Column(length = 10, precision = 2)
+	private double preco;
+
+	@NotBlank
+	@Size(min = 1, max = 50)
+	private String subcategoria;
+
+	public Long getIdProduto() {
+		return idProduto;
+	}
+
+	public void setIdProduto(Long idProduto) {
+		this.idProduto = idProduto;
+	}
+
+	public String getNomeProduto() {
+		return nomeProduto;
+	}
+
+	public void setNomeProduto(String nomeProduto) {
+		this.nomeProduto = nomeProduto;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+	public String getSubcategoria() {
+		return subcategoria;
+	}
+
+	public void setSubcategoria(String subcategoria) {
+		this.subcategoria = subcategoria;
+	}
+
+}
