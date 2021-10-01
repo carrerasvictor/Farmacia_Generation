f543406c3fd4e3201edff6dcee4bccaeb3220ab0
Victor Carreras <titotortuga3@gmail.com>
2021-10-01 15:54:06 -0300

    [Feature Victor - Task Farma: initial Commit CREATE app.properties] 2021.10.01

---
+++ genFarma/src/main/java/com/genFarma/genFarma/GenFarmaApplication.java
@@ -0,0 +1,13 @@
+package com.genFarma.genFarma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenFarmaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenFarmaApplication.class, args);
+	}
+
+}
---
+++ genFarma/src/test/java/com/genFarma/genFarma/GenFarmaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.genFarma.genFarma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenFarmaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
