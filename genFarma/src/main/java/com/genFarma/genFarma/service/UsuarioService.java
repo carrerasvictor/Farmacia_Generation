2a9d17cab9ab26953dcf60b65c9b1ca95e3e634b
davansep <priscila.davanse@gmail.com>
2021-10-08 17:54:25 -0300

    [@davansep - Task Farma: CREATE PACKAGE Service] 2021.10.08

---
+++ genFarma/src/main/java/com/genFarma/genFarma/service/UsuarioService.java
@@ -0,0 +1,123 @@
+package com.genFarma.genFarma.service;
+
+import java.nio.charset.Charset;
+import java.util.Optional;
+import org.apache.commons.codec.binary.Base64;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+import com.genFarma.genFarma.model.Usuario;
+import com.genFarma.genFarma.model.dtos.CredenciaisDTO;
+import com.genFarma.genFarma.model.dtos.UsuarioLoginDTO;
+import com.genFarma.genFarma.repository.UsuarioRepository;
+
+@Service
+public class UsuarioService {
+
+	private @Autowired UsuarioRepository repository;
+
+	/**
+	 * Metodo utilizado para cadastrar usuário validando duplicidade de email no
+	 * banco
+	 * 
+	 * @param usuarioParaCadastrar do tipo Usuario
+	 * @return Optional com Usuario cadastrado caso email não seja existente
+	 * @author Team Sinergia
+	 * @since 1.0
+	 * 
+	 */
+	public Optional<Object> cadastrarUsuario(Usuario usuarioParaCadastrar) {
+		return repository.findByEmail(usuarioParaCadastrar.getEmail()).map(usuarioExistente -> {
+			return Optional.empty();
+		}).orElseGet(() -> {
+			usuarioParaCadastrar.setSenha(encriptadorDeSenha(usuarioParaCadastrar.getSenha()));
+			return Optional.ofNullable(repository.save(usuarioParaCadastrar));
+		});
+	}
+
+	/**
+	 * Metodo utilizado para atualizar usuario no banco
+	 * 
+	 * @param usuarioParaAtualizar do tipo Usuario
+	 * @return Optional com Usuario atualizado
+	 * @author Team Sinergia
+	 * @since 1.0
+	 * 
+	 */
+	public Optional<Usuario> atualizarUsuario(Usuario usuarioParaAtualizar) {
+		return repository.findById(usuarioParaAtualizar.getIdUsuario()).map(resp -> {
+			resp.setNome(usuarioParaAtualizar.getNome());
+			resp.setSenha(encriptadorDeSenha(usuarioParaAtualizar.getSenha()));
+			return Optional.ofNullable(repository.save(resp));
+		}).orElseGet(() -> {
+			return Optional.empty();
+		});
+	}
+
+	/**
+	 * Metodo utilizado para pegar credenciais do usuario com Tokem (Formato Basic),
+	 * este método sera utilizado para retornar ao front o token utilizado para ter
+	 * acesso aos dados do usuario e mantelo logado no sistema
+	 * 
+	 * @param usuarioParaAutenticar do tipo UsuarioLoginDTO necessario email e senha
+	 *                              para validar
+	 * @return ResponseEntity com CredenciaisDTO preenchido com informações mais o
+	 *         Token
+	 * @since 1.0
+	 * @author Team Sinergia
+	 */
+	public ResponseEntity<CredenciaisDTO> pegarCredenciais(UsuarioLoginDTO usuarioParaAutenticar) {
+		return repository.findByEmail(usuarioParaAutenticar.getEmail()).map(resp -> {
+			BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+			if (encoder.matches(usuarioParaAutenticar.getSenha(), resp.getSenha())) {
+
+				CredenciaisDTO objetoCredenciaisDTO = new CredenciaisDTO();
+
+				objetoCredenciaisDTO
+						.setToken(gerarToken(usuarioParaAutenticar.getEmail(), usuarioParaAutenticar.getSenha()));
+				objetoCredenciaisDTO.setIdUsuario(resp.getIdUsuario());
+				objetoCredenciaisDTO.setNome(resp.getNome());
+				objetoCredenciaisDTO.setEmail(resp.getEmail());
+				objetoCredenciaisDTO.setSenha(resp.getSenha());
+
+				return ResponseEntity.status(201).body(objetoCredenciaisDTO); // Usuario Credenciado
+			} else {
+				throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Senha Incorreta!"); // Senha incorreta
+			}
+		}).orElseGet(() -> {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Email não existe!"); // Email não existe
+		});
+	}
+
+	/**
+	 * Metodo statico utilizado para gerar token
+	 * 
+	 * @param email
+	 * @param senha
+	 * @return Token no formato Basic para autenticação
+	 * @since 1.0
+	 * @author Team Sinergia
+	 */
+	private static String gerarToken(String email, String senha) {
+		String estruturaBasic = email + ":" + senha; // gustavoboaz@gmail.com:134652
+		byte[] estruturaBase64 = Base64.encodeBase64(estruturaBasic.getBytes(Charset.forName("US-ASCII"))); // hHJyigo-o+i7%0ÍUG465sas=-
+		return "Basic " + new String(estruturaBase64); // Basic hHJyigo-o+i7%0ÍUG465sas=-
+	}
+
+	/**
+	 * Método estatico que recebe a senha do usuario o criptografa
+	 * 
+	 * @param senha
+	 * @return String da senha criptografada
+	 * @since 1.0
+	 * @author Team Sinergia
+	 */
+	private static String encriptadorDeSenha(String senha) {
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		return encoder.encode(senha);
+	}
+}
